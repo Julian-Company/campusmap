2fe853b26b4a70312a6b7ef62ee25413c4766b50
mpeccia72 <michaelmpeccia@gmail.com>
2024-04-10 13:31:38 -0500

    update

---
+++ src/components/Map/Map.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import "./Map.css"
+
+
+function Map() {
+    return (
+      <div className="map">
+        
+        Map will be here
+      </div>
+    );
+  }
+  
+  export default Map;
